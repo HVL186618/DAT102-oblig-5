0d399f05720e81d5df307b9ba3a1d23252f8287d
HVL186618 <186618@stud.hvl.no>
2025-04-24 13:20:59 +0200

    e9jchwuhfr

---
+++ DAT107 oblig 5/src/uke15/oppgave2.java
@@ -0,0 +1,25 @@
+package uke15;
+
+public class oppgave2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public <T> void skrivVerdier(T nedre, T ovre) {
+
+		BinaerTreNode rot;
+		skrivVerdierRek(rot, nedre, ovre);
+	}
+	private <T> void skrivVerdierRek(BinaerTreNode<T> t, T nedre, T ovre) {
+		// TODO Auto-generated method stub
+		if (t != null) {
+		}
+		skrivVerdierRek (t.getVenstre(), min, maks); 
+		if ((t.getElement().compareTo(min) >= 0) && (t.getElement().compareTo(maks) <= 0)) { 
+			System.out.print(t.getElement() + " ");
+		}
+		skrivVerdierRek(t.getHogre(), min, maks);
+		
+	}
+
+}
